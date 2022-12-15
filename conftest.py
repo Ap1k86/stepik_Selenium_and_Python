a6da87c7f7ab6c11012989bf4f12cd13e56cd728
mitor_ap1k86 <Ap1k86@yandex.ru>
2022-12-15 17:04:47 +0300

    Uploaded files from the course and corrected README.md

---
+++ conftest.py
@@ -0,0 +1,52 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_name',
+        action='store',
+        default='chrome',  # chrome
+        help='Choose browser: Chrome or FireFox'
+    ),
+    parser.addoption(
+        "--language",
+        action='store',
+        default=None,
+        help='Choose language: en, ru, fr or another.'
+    )
+
+
+# open and quit browser.
+@pytest.fixture(scope="function")
+def browser(request):
+
+    # Choose language.
+    user_language = request.config.getoption("language")
+    options = Options()
+
+    # choose browser.
+    browser_name = request.config.getoption("browser_name")
+    # browser = None
+
+    # open browser.
+    if browser_name.lower() == "chrome":
+        print("\nStart browser Chrome for test..!")
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name.lower() == "firefox":
+        print("\nStart browser FireFox for test..!")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    # waiting for the page to load.
+    browser.implicitly_wait(20)
+    yield browser
+
+    # quit browser.
+    print("\nquit browser..!")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+
+
+# button search class
+class TestFinalLesson3:
+
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+    def test_button_search(self, browser):
+        # open site
+        browser.get(self.link)
+
+        # find button. 1st way!           ↓ This 'S' is very important for assert to work.
+        find_button = browser.find_elements(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+        assert find_button, "!!! No 'Add to basket' buttons !!!"
+        sleep(30)
+
+        # find button. 2nd way.
+        # try:
+        #     browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+        # except NoSuchElementException:
+        #     print("\nNo add to cart button")
+        # print("\nThere is an add to cart button")
+
+# pytest --language=es test_items.py
